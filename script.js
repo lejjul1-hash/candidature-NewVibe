a2a439cc7d96d187824bb6e8be201c2bd9b7c2f9
lejjul1-hash <lejjul1@gmail.com>
2025-12-21 14:54:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const WEBHOOK = "https://discord.com/api/webhooks/1452297731316912310/hJ8GetRDlkSzFK1g9_1FEAycxy5CJc8CCnWsLUIrRBZVTBn1nuuhLRwhX0rz9oUqXl87";
 const ROLE_ID = "1451995468585242841";
 const ADMIN_CODE = "GTlifetontop1234";
-const WHITELIST_IP = "";
+const WHITELIST_IP = "91.174.237.40";
 
 // PAGE SYSTEM
 function nextStep(){ step1.style.display="none"; step2.style.display="block"; }
@@ -146,5 +146,6 @@ function clearAll(){
     localStorage.removeItem("candidatures");
     adminPanel.innerHTML = "<p style='color:#ff4444;font-weight:700'>Toutes les candidatures ont été supprimées.</p>";
 }
+
 
 
